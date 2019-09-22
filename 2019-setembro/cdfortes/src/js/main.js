5f0ee36b33914a31fba44e3cf68323069e338bbf
cdfortes <cdfortes@gmail.com>
2019-09-22 01:51:40 -0100

    keyboard configuration

---
+++ 2019-setembro/cdfortes/src/js/main.js
@@ -0,0 +1,12 @@
+const monitor = document.querySelector('.monitor');
+const keyboard = document.querySelector('.keyboard');
+
+window.onload = (event) => {
+	monitor.style.width = '100%';
+	monitor.style.height = '200px';
+	monitor.style.opacity = 1;
+
+	keyboard.style.width = '100%';
+	keyboard.style.height = '490px';
+	keyboard.style.opacity = 1;
+};
